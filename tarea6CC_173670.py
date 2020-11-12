e6c4c9610add4b734030e6145396ab2776cf7b0a
Lucio27MV <47005249+Lucio27MV@users.noreply.github.com>
2020-11-12 17:03:14 -0600

    Add files via upload

---
+++ tarea6CC_173670.py
@@ -0,0 +1,43 @@
+# Luciano Montes de Oca Villa
+# 173670
+# Tarea 6 - Computación Cuántica
+# Pregunta VI
+
+# Import the Cirq library
+import cirq
+
+# Inserta las variables x,y que se elijan
+x = '1'
+y = '1'
+
+bell_circ = cirq.Circuit()
+
+q0,q1 = cirq.LineQubit.range(2)
+bell_circ.append(cirq.H(q0))
+
+if x == '0' and y == '0':
+    bell_circ.append(cirq.CNOT(q0,q1))
+elif x == '0' and y == '1' :
+    bell_circ.append(cirq.CNOT(q0,q1))
+    bell_circ.append(cirq.X(q1))
+elif x == '1' and y == '0':
+    bell_circ.append(cirq.CNOT(q0,q1))
+    bell_circ.append(cirq.Z(q1))
+elif x == '1' and y == '1':
+    bell_circ.append(cirq.CNOT(q0,q1))
+    bell_circ.append(cirq.Y(q1))
+
+s = cirq.Simulator()
+
+print('Circuit')
+print(bell_circ)
+print('Simulate the circuit:')
+results = s.simulate(bell_circ)
+print(results)
+print()
+
+bell_circ.append(cirq.measure(q0, q1, key = 'z'))
+
+print('Sample the circuit:')
+samples = s.run(bell_circ, repetitions = 100)
+print(samples.histogram(key = 'z'))
